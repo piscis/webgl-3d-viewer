3a9fd1d95bd2a44d2abadb9db78ef3d33d31061e
Alexander Pirsig <self@pirsig.net>
2015-10-25 13:00:22 +0100

    ask how to adjust version number

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -4,8 +4,14 @@ import bump from 'gulp-bump';
 import runSequence from 'run-sequence';
 import source from 'vinyl-source-stream';
 import uglify from 'gulp-uglify';
+import tag_version from 'gulp-tag-version';
+import git from 'gulp-git';
+import filter from 'gulp-filter';
+import prompt from 'gulp-prompt';
 
 var gulp = gulpHelp(gulpMain);
+
+var version = {type: 'patch'};
 
 gulp.task('release:viewer', false, () => {
 
@@ -38,12 +44,35 @@ gulp.task('release:example', false, () => {
 });
 
 
-gulp.task('release:bump-version', false, function(){
-  gulp.src('./package.json')
-    .pipe(bump({type:'patch'}))
-    .pipe(gulp.dest('./'));
+gulp.task('release:tag', false, function(){
+
+  return gulp.src('./package.json')
+    .pipe(bump(version))
+    .pipe(gulp.dest('./'))
+    .pipe(git.commit('bumps package version'))
+    .pipe(filter('package.json'))
+    .pipe(tag_version());
 });
 
+gulp.task('release:tag:create-version', false, function(){
+
+  return gulp.src('*')
+    .pipe(prompt.prompt({
+      type: 'checkbox',
+      name: 'bump',
+      message: 'What type of release is it? (Patch: hotfix, Minor: Release, Major: Major release)',
+      choices: ['patch', 'minor', 'major']
+    },function(res){
+
+      if(res.bump.length > 0){
+        version.type = res.bump[0];
+      }
+
+    }));
+});
+
+
+
 gulp.task('release:build', false, (cb)=>{
-  return runSequence(['release:compress','release:viewer','release:example'], 'release:bump-version', cb);
+  return runSequence(['release:compress','release:viewer','release:example'], 'release:tag:create-version', 'release:tag', cb);
 });
