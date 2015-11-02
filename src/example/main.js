87486787f2b9dc4581ed2c8db8f724f9856c24a7
Alexander Pirsig <self@pirsig.net>
2015-11-02 16:18:58 +0100

    extend example with tooltip info

--- src/example/main.js
+++ src/example/main.js
@@ -1,1 +1,41 @@
+// Bootstrapping for example
+
+require('intro.js/minified/introjs.min.css');
+
+import {introJs} from 'intro.js/minified/intro.min.js';
+
 window.ModelViewer = require('./../viewer');
+
+window.introJs = introJs;
+
+$(()=>{
+
+  if (localStorage && localStorage.getItem('disableTooltip') !== "true") {
+
+    setTimeout(()=>{
+
+      $('.close-button').attr('data-intro', 'Click here to modify viewer settings');
+      const opts = {
+        tooltipPosition: 'bottom-left',
+        showBullets: false,
+        showStepNumbers: false,
+        disableInteraction: true
+      };
+
+      let intro = introJs();
+      let keys = Object.keys(opts);
+      for (let key in keys) {
+
+        if (key) {
+          intro = intro.setOption(keys[key], opts[keys[key]]);
+        }
+      }
+
+      intro.start();
+
+      localStorage.setItem('disableTooltip', true);
+
+    }, 1500);
+  }
+
+});
